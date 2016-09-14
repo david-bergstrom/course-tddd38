f8c319daf265becbaf172dad60b0100ad665364d
David Bergström <david-bergstrom@outlook.com>
2016-09-14 09:37:51 +0200

    Add example of alias declaration

---
+++ code/alias-declaration/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+using str = std::string;
+using Number = int;
+
+int main() {
+    str s = "Hello World!";
+    cout << s << endl; 
+
+    Number n = 10;
+}
