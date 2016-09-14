9e997781d99a4c6a70e3a4a41e9788c3b3e41288
David Bergström <david-bergstrom@outlook.com>
2016-09-14 09:30:24 +0200

    Improve casting examples

--- code/casting/main.cc
+++ code/casting/main.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// static_cast
+// static_cast, used between related types
 void sc() {
 	int x = 5;
 
@@ -13,14 +13,65 @@ void sc() {
 	// Casting to a list
 	vector<int> v = static_cast<vector<int>>(x);
 
-	cout << v.size() << endl;
+	cout << "Vector size: " << v.size() << endl;
 }
 
-// const_cast
+// const_cast, removes constant
 void cc() {
-	const int always_same_number = 5;
+	int always_same_number = 5;
+	
+	int const & number = always_same_number;
+
+	int & another_reference = const_cast<int &>(number);
+
+	cout << "Before change: " << number << endl;
+
+	another_reference = 10;
+
+	cout << "After change: " << number << endl;
+}
+
+// dynamic_cast, withing a polymorphic class hierarchy
+void dc() {
+	class A {
+		public:
+			// Having a virtual method makes the class polymorphic. Also,
+			// having a virtual destructor helps, as this forces C++ to use the
+			// deconstructor in the child-classes instead, allowing them to
+			// clean up any allocated memory.
+			virtual ~A() {};
+	};
+
+	class B : public A {
+		public:
+			void print() {
+				cout << "What's up?? I'm B!" << endl;
+			}
+	};
+
+	class C : public A {
+		public:
+			void print() {
+				cout << "What's up?? I'm C!" << endl;
+			}
+	};
+
+	vector<A*> v;
+	v.push_back(new B);
+	v.push_back(new C);
+
+	for (A* object : v) {
+		if (B* b = dynamic_cast<B*>(object)) {
+			b->print();
+		} else if(C* c = dynamic_cast<C*>(object)) {
+			c->print();
+		}
+		delete object;
+	}
 }
 
 int main() {
-
+	sc();
+	cc();
+	dc();
 }
