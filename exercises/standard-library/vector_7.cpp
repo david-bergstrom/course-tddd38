707db3420935ada0ed66408d2e463f7334d410b4
David Bergström <david-bergstrom@outlook.com>
2016-12-30 20:01:56 +0100

    Replace do-while with functor and for_each

--- exercises/standard-library/vector_7.cpp
+++ exercises/standard-library/vector_7.cpp
@@ -1,20 +1,30 @@
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <vector>
+
+struct found_in {
+
+    found_in(std::vector<int> v) : v(v) {
+
+    }
+
+    void operator()(int i) {
+        if (std::binary_search(v.begin(), v.end(), i, std::greater<int>()))
+            std::cout << i << " was found!" << std::endl;
+        else
+            std::cout << i << " was not found!" << std::endl;
+    }
+
+    std::vector<int> v;
+};
 
 int main() {
     std::vector<int> v { 89, 8, 6, 23 };
 
     std::sort(v.begin(), v.end(), std::greater<int>());
 
-    int value;
-    do {
-        std::cin >> value;
-        if (std::binary_search(v.begin(), v.end(), value, std::greater<int>()))
-            std::cout << value << " was found!" << std::endl;
-        else
-            std::cout << value << " was not found!" << std::endl;
-    } while(value != -1);
+    std::for_each(std::istream_iterator<int>(std::cin), std::istream_iterator<int>(), found_in(v));
 
     return 0;
 }
